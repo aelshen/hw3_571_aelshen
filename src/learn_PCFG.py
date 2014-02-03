0021a02823ab09d05821cf4b2da2f37725988ce7
aelshen <ahmad.k.elshenawy@gmail.com>
2014-02-03 15:24:56 -0800

    Rule extraction complete (afaik), need to compute probabilities

--- src/learn_PCFG.py
+++ src/learn_PCFG.py
@@ -15,53 +15,82 @@ from collections import defaultdict
 #--------------------------------Constants-------------------------------------
 #==============================================================================
 DEBUG = True
-NONTERMINAL = re.compile(r"\(.*\)")
 TERMINAL = re.compile(r"\((\w+)\s+([\w?!.,;:]+)\)")
 #==============================================================================
 #-----------------------------------Main---------------------------------------
 #==============================================================================
 def main():
-    LHS_count = defaultdict(int)
-    cfg = defaultdict(lambda: defaultdict(int))
     x = "(TOP (SQ (SQ' (VBZ Does) (NP (DT this) (NN flight))) (VP (VB serve) (NP_NN dinner))) (PUNC ?))"
-    m = TERMINAL.findall(x)
-    for terminal in m:
-        LHS,RHS = terminal
-        LHS_count[LHS] += 1
-        cfg[LHS][RHS] += 1
-        
-    ExtractNonterminals(x)
+    hw3 = PCFG(sys.argv[1])
     
     print("Hello, World!")
 #==============================================================================    
 #---------------------------------Functions------------------------------------
 #==============================================================================
-def ExtractNonterminals(line):
-    line = line.replace("(","( ").replace(")",") ").split()
-    for i in range( len(line) ):
-        if line[i] == "(":
-            open_paren = 0
-            LHS = line[i + 1]
-            RHS = []
-            for j in range( i+1, len(line) ):
-                if line[j] == '(':
-                    open_paren += 1
-                elif line[j] == ')':
-                    open_paren -= 1
-                else:
-                    continue
-                
-                if open_paren == 1:
-                    RHS.append( line[j+1] )
-    x = 1
+
 
 #==============================================================================    
 #----------------------------------Classes-------------------------------------
 #==============================================================================
-class Classname:
-    def __init__(self):
-        self.x = 0
-
+class PCFG:
+    def __init__(self, file):
+        self.LHS_count = defaultdict(int)
+        self.pcfg = defaultdict(lambda: defaultdict(float))
+        
+        self.CreateGrammar(file)
+        x = 1
+    
+    def CreateGrammar(self, file):
+        for line in open(file, 'r').readlines():
+            self.ExtractTerminals(line)
+            line = line.replace("(","( ").replace(")"," )").split()
+            self.ExtractNonterminals(line)
+        
+    def ExtractNonterminals(self, line):
+        for i in range( len(line) ):
+            if line[i] != "(":
+                continue
+            else:
+                open_paren = 0
+                LHS = line[i + 1]
+                RHS = []
+                start_index = []
+                end_index = []
+                for j in range( i+2, len(line) ):
+                    if line[j] == '(':
+                        open_paren += 1
+                        if open_paren == 1:
+                            RHS.append( line[j+1] )
+                            start_index.append( j )
+                    elif line[j] == ')':
+                        open_paren -= 1
+                        if open_paren == 0:
+                            end_index.append( j )
+                    else:
+                        continue
+                #end j in range( i+2, len(line) ):
+            
+            if RHS:
+                self.LHS_count[LHS] += 1
+                self.pcfg[LHS][tuple(RHS)] += 1
+                for i in range( len(RHS) ):
+                    start = start_index[i]
+                    end = end_index[i]
+                    self.ExtractNonterminals(line[start:end])
+            
+            
+            break
+                    
+                    
+        #end i in range( len(line) ):
+        
+        
+    def ExtractTerminals(self, line):
+        m = TERMINAL.findall(line)
+        for terminal in m:
+            LHS,RHS = terminal
+            self.LHS_count[LHS] += 1
+            self.pcfg[LHS][RHS] += 1
 #==============================================================================    
 #------------------------------------------------------------------------------
 #==============================================================================
